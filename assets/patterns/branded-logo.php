bf010d952ed89ad16e7858772097da7a2c2571a9
Filippo Di Trapani <filippodt@gmail.com>
2025-11-08 16:03:55 -0500

    Final touches on v0.1

---
+++ assets/patterns/branded-logo.php
@@ -0,0 +1,14 @@
+<?php
+   /**
+    * Title: Branded logo block
+    * Slug: newfolio/branded-logo
+    * Categories: featured, header
+    * Description: Display the branded logo template part
+    */
+   ?>
+   <!-- wp:group {"className":"branded-logo-container"} -->
+   <div class="wp-block-group branded-logo-container">
+        <!-- wp:template-part {"slug":"filippo-logo"} /-->
+   </div>
+   <!-- /wp:group -->
+
